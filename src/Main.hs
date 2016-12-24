414d5361eb27ed0c37cc93d1bffb3520a252db14
praveen <prrraveen@gmail.com>
2016-12-25 03:16:39 +0530

    url parms readed

--- src/Main.hs
+++ src/Main.hs
@@ -1,20 +1,46 @@
 {-# LANGUAGE OverloadedStrings #-}
 
 module Main where
-
+import Control.Monad
 import Network.Wai
 import Network.HTTP.Types
 import Network.Wai.Handler.Warp (run)
-
-app :: Application
-app _ respond = do
-    putStrLn "I've done some IO here"
-    respond $ responseLBS
-        status200
-        [("Content-Type", "text/plain")]
-        "Hello, Web!"
+import Data.ByteString
+import Control.Monad
+import System.Process
+import qualified Data.ByteString.Lazy.Char8 as L8
 
 main :: IO ()
 main = do
-    putStrLn $ "http://localhost:8080/"
     run 8080 app
+
+app :: Application
+app request respond = respond $ case rawPathInfo request of
+    "/" -> indexHtml 
+    "/wake" -> wakeMeUP request
+    _ -> fourNotFour
+
+indexHtml :: Response
+indexHtml = responseFile
+    status200
+    [("Content-Type","text/html")]
+    "index.html"
+    Nothing
+
+wakeMeUP :: Request -> Response
+wakeMeUP request = 
+    let query = queryString request
+        hour = join $ lookup "hour" query
+        min = join $ lookup "min" query
+ 
+    in responseLBS
+        status200
+        [("Content-Type","text/plain")]
+        "Alarm set at...to be coded later"
+
+fourNotFour :: Response
+fourNotFour = responseLBS
+    status404
+    [("Content-Type","text/plain")]
+    "404 not found"
+    
