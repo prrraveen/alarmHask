e924f75a1422c7176c70045dcc01b94ab5fe9799
praveen <prrraveen@gmail.com>
2016-12-26 22:38:39 +0530

    added comiled output

--- src/Main.hs
+++ src/Main.hs
@@ -1,5 +1,5 @@
-{-# LANGUAGE OverloadedStrings #-}
-
+{-# LANGUAGE OverloadedStrings  #-}
+{-# LANGUAGE QuasiQuotes, ExtendedDefaultRules #-}
 module Main where
 import Control.Monad
 import Network.Wai
@@ -9,6 +9,7 @@ import Data.ByteString
 import Control.Monad
 import System.Process
 import qualified Data.ByteString.Lazy.Char8 as L8
+import Text.InterpolatedString.Perl6 (q, qc)
 
 main :: IO ()
 main = do
@@ -19,6 +20,7 @@ app request respond = do
     response <- case rawPathInfo request of
                     "/" -> return indexHtml 
                     "/wake" -> wakeMeUP request
+                    "/stop" -> stop
                     _ -> return fourNotFour
     respond response
 
@@ -33,15 +35,31 @@ wakeMeUP :: Request -> IO Response
 wakeMeUP request = do 
     let query = queryString request
     let hour = join $ lookup "hour" query
-    runCommand "ls"
+    let min  = join $ lookup "min" query
+    runCommand $ job hour min
     return $ responseLBS
         status200
         [("Content-Type","text/plain")]
         "Alarm set at...to be coded later"
+
+stop :: IO Response
+stop = do
+    runCommand "mpc stop"
+    return $ responseLBS
+        status200
+        [("Content-Type","text/plain")]
+        "alarm stoped"
 
 fourNotFour :: Response
 fourNotFour = responseLBS
     status404
     [("Content-Type","text/plain")]
     "404 not found"
-    
+
+type Hour = Maybe ByteString
+type Minute = Maybe ByteString
+
+job :: Hour -> Minute -> String
+job Nothing _ = [q| echo "didn't get hour "  |]
+job (Just hr) Nothing =  [qc| echo "* {hr} * * * /usr/bin/mpc play" | crontab - |] 
+job (Just hr) (Just min) = [qc| echo "{min} {hr} * * * /usr/bin/mpc play" | crontab - |]
